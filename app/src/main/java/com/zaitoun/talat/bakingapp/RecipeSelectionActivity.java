c8273f83244e1ada4fb2d45cd6e9cde6c60a8c1e
Talat Zaitoun <talat.s.zaitoun@gmail.com>
2017-07-26 18:15:04 -0400

    Initial Commit

---
+++ app/src/main/java/com/zaitoun/talat/bakingapp/RecipeSelectionActivity.java
@@ -0,0 +1,13 @@
+package com.zaitoun.talat.bakingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_selection);
+    }
+}
